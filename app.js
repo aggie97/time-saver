91a686be435b1b449321ff2b10c33c47e276dd6b
aggie <4ggie1997@gmail.com>
2022-07-04 13:50:54 +0900

    홈, 투두, 랜덤 아이콘 추가 & nav 스크롤 시  sticky 기능과 그에 따른 style 추가

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+const navBar = document.querySelector(".nav-bar");
 const todoOption = document.querySelector("#todo");
 const gachaOption = document.querySelector("#gacha");
 const todo = document.querySelector(".todo");
 const gacha = document.querySelector(".random-meal");
+
+const observer = new IntersectionObserver(
+  ([e]) => e.target.classList.toggle("isSticky", e.intersectionRatio < 1),
+  { threshold: [1] }
+);
+
+observer.observe(navBar);
 
 const triggerTodo = (event) => {
   todo.hidden = false;
